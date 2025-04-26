c107b2421ce282374f2028758965b08a3f655c9b
Your Name <tangshirong477@gmail.com>
2025-04-27 02:16:55 +0800

    start

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>2505 Forever</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          小时时薪 + {count}
+        </button>
+        <p>
+          点击加薪，自己的工资自己努力争取
+        </p>
+      </div>
+      <p className="read-the-docs">
+        世袭制才情，传女不传男
+      </p>
+    </>
+  )
+}
+
+export default App
